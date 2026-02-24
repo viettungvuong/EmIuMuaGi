87b865f1f08067b4c53ceebc4b4905fb5d33b0a8
viettungvuong <viettungvuong@gmail.com>
2026-02-24 21:35:18 +0700

    Change site running BE

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:8000",
-  headers: { "Content-Type": "application/json" },
+  baseURL: import.meta.env.VITE_API_URL || "https://emiumuagi.onrender.com",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default client;

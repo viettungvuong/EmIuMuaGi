601dc1d58a0de5d80fe27867e40e9c258f9733ef
viettungvuong <viettungvuong@gmail.com>
2026-02-25 23:08:22 +0700

    Change url

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const client = axios.create({
   baseURL:
-    import.meta.env.VITE_API_URL || "https://b269-115-73-138-27.ngrok-free.app",
+    import.meta.env.VITE_API_URL || "https://c188-115-73-138-27.ngrok-free.app",
   headers: {
     "Content-Type": "application/json",
     "ngrok-skip-browser-warning": "true",

b09539d29f60d10aa591fb663024f00f41f18b9e
viettungvuong <viettungvuong@gmail.com>
2026-03-15 21:21:52 +0700

    Update history fetch

--- backend/item-service/handlers/history.go
+++ backend/item-service/handlers/history.go
@@ -2,7 +2,6 @@ package handlers
 
 import (
 	"net/http"
-	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/google/uuid"
@@ -11,17 +10,25 @@ import (
 
 func GetHistories(c *gin.Context) {
 	var res []struct {
-		ID     uuid.UUID `json:"id"`
-		ItemID uint      `json:"item_id"`
-		Time   time.Time `json:"time"`
+		ID       uuid.UUID `json:"id"`
+		ItemID   uint      `json:"item_id"`
+		ItemName string    `json:"item_name"`
+		Score    *int      `json:"score"`
+		Content  *string   `json:"content"`
 	}
 
 	err := database.DB.Raw(`
-	SELECT * FROM histories h
+		SELECT h.id, h.item_id, i.item_name, h.time, r.score, r.content
+		FROM histories h
+		LEFT JOIN items i ON h.item_id = i.id
+		LEFT JOIN reviews r ON h.id = r.history_id
+		ORDER BY h.time DESC
 	`).Scan(&res).Error
 
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to retrieve histories"})
 		return
 	}
+
+	c.JSON(http.StatusOK, res)
 }

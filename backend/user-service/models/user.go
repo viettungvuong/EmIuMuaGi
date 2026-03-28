a350f54b1a10c17d3b604501c7288fbe08962643
viettungvuong <viettungvuong@gmail.com>
2026-03-28 20:48:27 +0700

    Invite link

--- backend/user-service/models/user.go
+++ backend/user-service/models/user.go
@@ -1,9 +1,20 @@
 package models
 
+import (
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
 type User struct {
-	ID        string `gorm:"primaryKey" json:"id"`
-	Email     string `gorm:"unique;not null" json:"email"`
-	Password  string `gorm:"not null" json:"-"`
-	PartnerID *string `json:"partner_id"`
-	Partner   *User   `gorm:"foreignKey:PartnerID" json:"partner,omitempty"`
+	ID         string  `gorm:"primaryKey" json:"id"`
+	Email      string  `gorm:"unique;not null" json:"email"`
+	Password   string  `gorm:"not null" json:"-"`
+	PartnerID  *string `json:"partner_id"`
+	Partner    *User   `gorm:"foreignKey:PartnerID" json:"partner,omitempty"`
+	InviteLink string  `gorm:"not null" json:"invite_link"`
+}
+
+func (u *User) BeforeCreate(tx *gorm.DB) (err error) {
+	u.InviteLink = uuid.New().String()
+	return
 }

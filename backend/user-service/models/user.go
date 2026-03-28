cc442a72de8a0b906a44df5e9c3ebb75bf4ce90e
viettungvuong <viettungvuong@gmail.com>
2026-03-28 20:40:09 +0700

    Partner

--- backend/user-service/models/user.go
+++ backend/user-service/models/user.go
@@ -1,7 +1,9 @@
 package models
 
 type User struct {
-	ID       string `gorm:"primaryKey" json:"id"`
-	Email    string `gorm:"unique;not null" json:"email"`
-	Password string `gorm:"not null" json:"-"`
+	ID        string `gorm:"primaryKey" json:"id"`
+	Email     string `gorm:"unique;not null" json:"email"`
+	Password  string `gorm:"not null" json:"-"`
+	PartnerID *string `json:"partner_id"`
+	Partner   *User   `gorm:"foreignKey:PartnerID" json:"partner,omitempty"`
 }

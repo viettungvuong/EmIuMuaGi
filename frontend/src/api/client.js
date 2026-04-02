df34d048328e10ea61c3051999fc7730cb1cef2d
viettungvuong <viettungvuong@gmail.com>
2026-04-02 22:21:56 +0700

    Client

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
 const client = axios.create({
-  baseURL:
-    import.meta.env.VITE_API_URL ||
-    "https://nonspherical-ethelene-pangenetically.ngrok-free.dev",
+  baseURL: "/",
   headers: {
     "Content-Type": "application/json",
     "ngrok-skip-browser-warning": "true",
